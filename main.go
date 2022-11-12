0e437f7b2b6bf3ef1c5e208bbf761d51f3570e14
fujitoshion <43021337+FUJITOSHION@users.noreply.github.com>
2022-11-13 00:23:01 +0900

    auto commit

--- main.go
+++ main.go
@@ -1,19 +1,1 @@
 package main
-
-import "fmt"
-
-type Adder struct {
-	start int
-}
-
-func (a Adder) AddTo(val int) int {
-	return a.start + val
-}
-
-func main() {
-	myAdder10 := Adder{start: 10}
-	fmt.Println(myAdder10.AddTo(5)) //15
-
-	f1 := myAdder10.AddTo
-	fmt.Println(f1(10)) //20
-}
