3e7a27983634e29d829ed916b0c1c5350432b3bc
fujitoshion <43021337+FUJITOSHION@users.noreply.github.com>
2022-11-13 22:27:35 +0900

    auto commit

--- main.go
+++ main.go
@@ -10,11 +10,17 @@ func process(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, r.Form)
 }
 
+func writeExample(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(501)
+	fmt.Fprintln(w, "501")
+}
+
 func main() {
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
 	}
 	http.HandleFunc("/process", process)
+	http.HandleFunc("/writeheader", writeExample)
 	server.ListenAndServe()
 
 }
