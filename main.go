0544caa9b580b5f6095dba5955d4c6ce8f478395
fujitoshion <43021337+FUJITOSHION@users.noreply.github.com>
2022-11-13 20:21:47 +0900

    auto commit

--- main.go
+++ main.go
@@ -5,27 +5,16 @@ import (
 	"net/http"
 )
 
-type HelloHandler struct{}
-
-func (h *HelloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello!")
-}
-
-type WorldHandler struct{}
-
-func (h *WorldHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "World!")
+func headers(w http.ResponseWriter, r *http.Request) {
+	h := r.Header
+	fmt.Fprintln(w, h)
 }
 
 func main() {
-	hello := HelloHandler{}
-	world := WorldHandler{}
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
 	}
-
-	http.Handle("/hello", &hello)
-	http.Handle("/world", &world)
+	http.HandleFunc("/headers", headers)
 
 	server.ListenAndServe()
 }
