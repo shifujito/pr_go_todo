9e7d37d87d13befaf0ddb4ea80b591c7de90897c
fujitoshion <43021337+FUJITOSHION@users.noreply.github.com>
2022-11-13 22:24:42 +0900

    auto commit

--- main.go
+++ main.go
@@ -5,16 +5,16 @@ import (
 	"net/http"
 )
 
-func headers(w http.ResponseWriter, r *http.Request) {
-	h := r.Header
-	fmt.Fprintln(w, h)
+func process(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	fmt.Fprintln(w, r.Form)
 }
 
 func main() {
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
 	}
-	http.HandleFunc("/headers", headers)
-
+	http.HandleFunc("/process", process)
 	server.ListenAndServe()
+
 }
