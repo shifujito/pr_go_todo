1c5e3107b77abeed05e683ec545db98bb4034ad3
fujitoshion <43021337+FUJITOSHION@users.noreply.github.com>
2022-11-20 23:40:41 +0900

    auto commit

--- main.go
+++ main.go
@@ -1,26 +1,35 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-)
+import "fmt"
 
-func process(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-	fmt.Fprintln(w, r.Form)
+type Post struct {
+	Id      int
+	Content string
+	Author  string
 }
 
-func writeExample(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(501)
-	fmt.Fprintln(w, "501")
+var PostById map[int]*Post
+var PostByAuthor map[string][]*Post
+
+func store(post Post) {
+	PostById[post.Id] = &post
+	PostByAuthor[post.Author] = append(PostByAuthor[post.Author], &post)
 }
 
 func main() {
-	server := http.Server{
-		Addr: "127.0.0.1:8080",
-	}
-	http.HandleFunc("/process", process)
-	http.HandleFunc("/writeheader", writeExample)
-	server.ListenAndServe()
+	PostById = make(map[int]*Post)
+	PostByAuthor = make(map[string][]*Post)
 
+	post1 := Post{Id: 1, Content: "Hello World!", Author: "Fujito"}
+	post2 := Post{Id: 2, Content: "Hoge", Author: "Hoge"}
+
+	store(post1)
+	store(post2)
+
+	for _, post := range PostByAuthor["Fujito"] {
+		fmt.Println(post)
+	}
+	for _, post := range PostByAuthor["Hoge"] {
+		fmt.Println(post)
+	}
 }
