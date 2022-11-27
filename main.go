7ee5c7c2d251048cc4ec4ee2e258a23ca74ab441
fujitoshion <43021337+FUJITOSHION@users.noreply.github.com>
2022-11-27 13:02:44 +0900

    auto commit

--- main.go
+++ main.go
@@ -1,35 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
+)
 
-type Post struct {
-	Id      int
-	Content string
-	Author  string
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
 }
 
-var PostById map[int]*Post
-var PostByAuthor map[string][]*Post
-
-func store(post Post) {
-	PostById[post.Id] = &post
-	PostByAuthor[post.Author] = append(PostByAuthor[post.Author], &post)
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, nil)
 }
 
 func main() {
-	PostById = make(map[int]*Post)
-	PostByAuthor = make(map[string][]*Post)
-
-	post1 := Post{Id: 1, Content: "Hello World!", Author: "Fujito"}
-	post2 := Post{Id: 2, Content: "Hoge", Author: "Hoge"}
-
-	store(post1)
-	store(post2)
-
-	for _, post := range PostByAuthor["Fujito"] {
-		fmt.Println(post)
-	}
-	for _, post := range PostByAuthor["Hoge"] {
-		fmt.Println(post)
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe", err)
 	}
 }
